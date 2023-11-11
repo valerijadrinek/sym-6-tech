feea8b747a8ca3e8e6b05ae3130fd091e2d0a12c
valerijadrinek <valerijadrinek@gmail.com>
2023-11-11 21:36:07 +0100

    song api endpoint

--- src/Controller/SongApiController.php
+++ src/Controller/SongApiController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SongApiController extends AbstractController
 {
-    #[Route('/song/api', name: 'app_song_api')]
-    public function index(): Response
+    #[Route('/api/song/{id<\d+>}', name: 'app_api_song', methods: ['GET'])]
+    public function getSong(int $id, LoggerInterface $logger): Response
     {
-        return $this->render('song_api/index.html.twig', [
-            'controller_name' => 'SongApiController',
-        ]);
+        $song = [
+            'id' => $id,
+            'name' => 'Waterfalls',
+            'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
+        ];
+
+        $logger->info('Returning API response for song {song}', 
+    [
+        'song' => $id
+    ]);
+
+        return $this->json($song);
     }
 }
