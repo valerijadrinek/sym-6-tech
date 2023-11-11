d471aab618d7a5a9387e1a622718abede18b7c87
valerijadrinek <valerijadrinek@gmail.com>
2023-11-11 20:03:52 +0100

    twig templates added

--- src/Controller/VinylSongsController.php
+++ src/Controller/VinylSongsController.php
@@ -8,11 +8,34 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class VinylSongsController extends AbstractController
 {
-    #[Route('/vinyl/songs', name: 'app_vinyl_songs')]
-    public function index(): Response
+    #[Route('/', name: 'app_home')]
+    public function homepage(): Response
     {
-        return $this->render('vinyl_songs/index.html.twig', [
-            'controller_name' => 'VinylSongsController',
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+        ];
+
+        
+        return $this->render('vinyl/index.html.twig', [
+            'title' => 'PB & Jams',
+            'tracks' => $tracks,
         ]);
+    }
+
+    #[Route('/browse/{slug}', name: "app_browse")]
+    public function browse(string $slug = null): Response
+    {
+        if ($slug) {
+            $title = 'Genre: '. str_replace('-', ' ', $slug);
+        } else {
+            $title = 'All Genres';
+        }
+        return new Response($title);
+        
     }
 }
