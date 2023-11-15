87b248ba3dbaa8d9029f85655b467b4f4be3e9f1
valerijadrinek <valerijadrinek@gmail.com>
2023-11-15 14:53:08 +0100

    isDebug added

--- src/Service/MixRepository.php
+++ src/Service/MixRepository.php
@@ -4,18 +4,26 @@ namespace App\Service;
 use Psr\Cache\CacheItemInterface;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 class MixRepository
 {
     public function __construct(
-        private HttpClientInterface $httpClient,
-        private CacheInterface $cache
+        private HttpClientInterface $githubContentClient,
+        private CacheInterface $cache,
+        #[Autowire('%kernel.debug%')]
+        private bool $isDebug
     ) {}
     public function findAll(): array
     {
         return $this->cache->get('mixes_data', function(CacheItemInterface $cacheItem) {
-            $cacheItem->expiresAfter(5);
-            $response = $this->httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');
+            $cacheItem->expiresAfter($this->isDebug ?  60 : 6000);
+            $response = $this->githubContentClient->request('GET', '/SymfonyCasts/vinyl-mixes/main/mixes.json', 
+            [
+                'headers' => [
+                    'Authorization' => 'Token ghp_foo_bar',
+                ] ]
+        );
             return $response->toArray();
         });
     }
