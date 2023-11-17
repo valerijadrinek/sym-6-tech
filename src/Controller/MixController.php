fc90bac9aa78e31bdbcb236842c7843ed98b2a8e
valerijadrinek <valerijadrinek@gmail.com>
2023-11-17 22:00:40 +0100

    flash message

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -49,6 +49,7 @@ class MixController extends AbstractController
             $mix->setVotes($mix->getVotes() - 1);
         }
         $em->flush();
+        $this->addFlash('success', 'Vote counted!');
         
         return $this->redirectToRoute('app_mix_show', [
             'id' => $mix->getId(),
